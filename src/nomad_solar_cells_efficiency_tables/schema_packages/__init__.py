1b52811b6fc5ef37dc1fe4e55798a711a41628fc
Pepe Marquez <64071335+Pepe-Marquez@users.noreply.github.com>
2024-05-15 13:02:56 +0000

    Changed entry points

--- src/nomad_solar_cells_efficiency_tables/schema_packages/__init__.py
+++ src/nomad_solar_cells_efficiency_tables/schema_packages/__init__.py
@@ -6,12 +6,30 @@ class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from nomad_solar_cells_efficiency_tables.schema_packages.mypackage import m_package
- 
+        from nomad_solar_cells_efficiency_tables.schema_packages.mypackage import (
+            m_package,
+        )
+
         return m_package
+
+class EfficiencyTablesEntryPoint(SchemaPackageEntryPoint):
+
+    def load(self):
+        from nomad_solar_cells_efficiency_tables.schema_packages.efficiency_tables import (
+            m_package,
+        )
+
+        return m_package
+
+
+efficiency_tables = EfficiencyTablesEntryPoint(
+    name='Efficiency Tables',
+    description='Schema package for describing data from efficiency tables of solar cells.',
+)
 
 
 mypackage = MySchemaPackageEntryPoint(
     name='MyPackage',
     description='Schema package defined using the new plugin mechanism.',
 )
+
